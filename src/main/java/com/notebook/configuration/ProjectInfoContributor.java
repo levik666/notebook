2c0a049cc82c93463c40598cfdd3e97d99a0e333
Yevgen Polukov <yevgen.polukov@paycash.onmicrosoft.com>
2017-07-03 20:56:42 +0300

    add git information in /info.

--- src/main/java/com/notebook/configuration/ProjectInfoContributor.java
+++ src/main/java/com/notebook/configuration/ProjectInfoContributor.java
@@ -4,16 +4,19 @@ import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
-public class ProjectInfoContributor implements InfoContributor{
+public class ProjectInfoContributor implements InfoContributor {
 
-    private String version;
-
-    public ProjectInfoContributor() {
-    }
+    private static final String TIME = "Current local time with time zone";
 
     @Override
     public void contribute(Info.Builder builder) {
-        builder.withDetail("project", version);
+        Map<String, String> project = new HashMap<>();
+        project.put(TIME, ZonedDateTime.now().toString());
+        builder.withDetail("project", project);
     }
 }
